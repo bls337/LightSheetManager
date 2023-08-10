10a53413ed6a6af6388609d2a172bc03d899fe7c
Brandon <brandon@asiimaging.com>
2023-08-10 12:21:59 -0700

    add NIDAQ class

---
+++ src/main/java/org/micromanager/lightsheetmanager/model/devices/NIDAQ.java
@@ -0,0 +1,16 @@
+package org.micromanager.lightsheetmanager.model.devices;
+
+import org.micromanager.Studio;
+
+/**
+ *
+ * https://micro-manager.org/NIDAQ
+ */
+public class NIDAQ extends DeviceBase {
+
+    public NIDAQ(final Studio studio, final String deviceName) {
+        super(studio, deviceName);
+    }
+
+
+}
