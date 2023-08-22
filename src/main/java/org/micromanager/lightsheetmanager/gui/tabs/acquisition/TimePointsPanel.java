fdcee55e2a0c842574a485bf138d325855a3beb1
Brandon <brandon@asiimaging.com>
2023-08-22 15:27:28 -0700

    move timepoints into panel class

--- src/main/java/org/micromanager/lightsheetmanager/gui/tabs/acquisition/TimePointsPanel.java
+++ src/main/java/org/micromanager/lightsheetmanager/gui/tabs/acquisition/TimePointsPanel.java
@@ -1,19 +1,75 @@
 package org.micromanager.lightsheetmanager.gui.tabs.acquisition;
 
+import org.micromanager.lightsheetmanager.api.internal.DefaultAcquisitionSettingsDISPIM;
+import org.micromanager.lightsheetmanager.gui.components.CheckBox;
+import org.micromanager.lightsheetmanager.gui.components.Label;
 import org.micromanager.lightsheetmanager.gui.components.Panel;
+import org.micromanager.lightsheetmanager.gui.components.Spinner;
+import org.micromanager.lightsheetmanager.model.LightSheetManagerModel;
 
-// TODO: use this?
+import java.util.Objects;
+
 public class TimePointsPanel extends Panel {
 
-    public TimePointsPanel() {
-        init();
+    private Label lblNumTimePoints_;
+    private Label lblTimePointInterval_;
+    private Spinner spnNumTimePoints_;
+    private Spinner spnTimePointInterval_;
+
+    private final LightSheetManagerModel model_;
+
+    public TimePointsPanel(final LightSheetManagerModel model, final CheckBox cbxUseTimePoints) {
+        super(cbxUseTimePoints);
+        model_ = Objects.requireNonNull(model);
+        createUserInterface();
+        createEventHandlers();
     }
 
-    private void init() {
+    private void createUserInterface() {
+        final DefaultAcquisitionSettingsDISPIM acqSettings =
+                model_.acquisitions().getAcquisitionSettings();
 
+        setMigLayout(
+                "",
+                "[]5[]",
+                "[]5[]"
+        );
+
+        lblNumTimePoints_ = new Label("Number:");
+        lblTimePointInterval_ = new Label("Interval [s]:");
+        spnNumTimePoints_ = Spinner.createIntegerSpinner(
+                acqSettings.numTimePoints(), 1, Integer.MAX_VALUE,1);
+        spnTimePointInterval_ = Spinner.createIntegerSpinner(
+                acqSettings.timePointInterval(), 0, Integer.MAX_VALUE, 1);
+
+        add(lblNumTimePoints_, "");
+        add(spnNumTimePoints_, "wrap");
+        add(lblTimePointInterval_, "");
+        add(spnTimePointInterval_, "");
     }
 
+    // TODO: update duration labels
     private void createEventHandlers() {
+        final DefaultAcquisitionSettingsDISPIM.Builder asb_ =
+                model_.acquisitions().getAcquisitionSettingsBuilder();
 
+        spnNumTimePoints_.registerListener(e -> {
+            asb_.numTimePoints(spnNumTimePoints_.getInt());
+            //updateDurationLabels();
+            //System.out.println("getNumTimePoints: " + model_.acquisitions().getAcquisitionSettings().getNumTimePoints());
+        });
+
+        spnTimePointInterval_.registerListener(e -> {
+            asb_.timePointInterval(spnTimePointInterval_.getInt());
+            //updateDurationLabels();
+            //System.out.println("getTimePointInterval: " + model_.acquisitions().getAcquisitionSettings().getTimePointInterval());
+        });
+    }
+
+    public void setEnabled(final boolean state) {
+        lblNumTimePoints_.setEnabled(state);
+        lblTimePointInterval_.setEnabled(state);
+        spnNumTimePoints_.setEnabled(state);
+        spnTimePointInterval_.setEnabled(state);
     }
 }
