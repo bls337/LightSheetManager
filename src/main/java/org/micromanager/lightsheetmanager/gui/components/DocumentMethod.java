e9e3974ada46f15c437a5963e754f1868163e6b6
Brandon Simpson <brandon@asiimaging.com>
2023-09-21 23:01:32 -0700

    remove unused components

--- src/main/java/org/micromanager/lightsheetmanager/gui/components/DocumentMethod.java
+++
@@ -1,8 +0,0 @@
-package org.micromanager.lightsheetmanager.gui.components;
-
-import javax.swing.event.DocumentEvent;
-
-@FunctionalInterface
-public interface DocumentMethod {
-    void run(DocumentEvent event);
-}
