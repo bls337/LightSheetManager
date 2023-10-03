f4bed695a6f192b14fffcfbf854be805737add2f
Brandon <brandon@asiimaging.com>
2023-10-02 17:48:55 -0700

    make channel table preset a combo box

--- src/main/java/org/micromanager/lightsheetmanager/gui/tabs/channels/ChannelTable.java
+++ src/main/java/org/micromanager/lightsheetmanager/gui/tabs/channels/ChannelTable.java
@@ -4,9 +4,12 @@ import org.micromanager.lightsheetmanager.model.LightSheetManagerModel;
 import org.micromanager.lightsheetmanager.model.channels.ChannelSpec;
 import org.micromanager.lightsheetmanager.model.channels.ChannelTableData;
 
+import javax.swing.DefaultCellEditor;
+import javax.swing.JComboBox;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
+import javax.swing.table.TableColumn;
 import java.util.Objects;
 
 public class ChannelTable extends JScrollPane {
@@ -25,6 +28,13 @@ public class ChannelTable extends JScrollPane {
         tableData_ = new ChannelTableData(channels);
         tableModel_ = new ChannelTableModel(tableData_);
         table_ = new JTable(tableModel_);
+
+        // init presets combo box
+        TableColumn column = table_.getColumnModel().getColumn(1);
+        JComboBox<String> cmbPresets = new JComboBox<>();
+        cmbPresets.addItem("None");
+        cmbPresets.setSelectedIndex(0);
+        column.setCellEditor(new DefaultCellEditor(cmbPresets));
 
         // cancel JTable edits when focus is lost to prevent errors
         table_.putClientProperty("terminateEditOnFocusLost", Boolean.TRUE);
