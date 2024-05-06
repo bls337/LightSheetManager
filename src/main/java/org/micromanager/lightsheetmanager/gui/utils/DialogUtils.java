0b91ea49945c626336bebe9f24630513096f4545
Brandon <brandon@asiimaging.com>
2024-05-06 16:07:22 -0700

    add dialog box to reset speed

--- src/main/java/org/micromanager/lightsheetmanager/gui/utils/DialogUtils.java
+++ src/main/java/org/micromanager/lightsheetmanager/gui/utils/DialogUtils.java
@@ -4,8 +4,11 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 
+// TODO: is errorLog the best way to send errors to JTextArea? reconsider impl for AcquisitionTable
+
 /**
- * This is primarily used in the acquisition playlist ui.
+ * A utility class for making dialog boxes.
+ *
  */
 public class DialogUtils {
 
@@ -14,9 +17,6 @@ public class DialogUtils {
 
     /**The component to display the errors. */
     private static JTextArea errorLog = null;
-
-    public DialogUtils() {
-    }
 
     /**
      * Sets the JTextArea to log errors.
@@ -27,11 +27,27 @@ public class DialogUtils {
         errorLog = textArea;
     }
 
+    /**
+     * Return the user's input or null if the action is canceled.
+     *
+     * @param frame the parent frame
+     * @param title the title string
+     * @param message the message to display
+     * @return the user's input or {@code null} meaning the user canceled the input
+     */
     public static String showTextEntryDialog(final JFrame frame, final String title, final String message) {
         return (String) JOptionPane.showInputDialog(frame, message, title,
                 JOptionPane.PLAIN_MESSAGE, null, null, "");
     }
 
+    /**
+     * Return 0 if "Yes" is selected or 1 if "No" is selected.
+     *
+     * @param frame the parent frame
+     * @param title the title string
+     * @param message the message to display
+     * @return an int indicating the option selected by the user.
+     */
     public static int showYesNoDialog(final JFrame frame, final String title, final String message) {
         return JOptionPane.showConfirmDialog(frame, message, title, JOptionPane.YES_NO_OPTION);
     }
@@ -40,8 +56,8 @@ public class DialogUtils {
      * Shows a customized message dialog box, this method does not log the error.<P>
      * This is used for reporting errors in the AcquisitionTable.
      *
-     * @param frame the frame in which the dialog is displayed
-     * @param title the title string for the dialog
+     * @param frame the parent frame
+     * @param title the title string
      * @param message the message to display
      */
     public static void showErrorMessage(final JFrame frame, final String title, final String message) {
