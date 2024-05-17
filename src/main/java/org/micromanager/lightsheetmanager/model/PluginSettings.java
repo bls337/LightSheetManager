0c297191c861d25b7f9aafd774680bb3194e3f8e
bls337 <bls337@yahoo.com>
2024-05-16 17:57:27 -0700

    Add files via upload

---
+++ src/main/java/org/micromanager/lightsheetmanager/model/PluginSettings.java
@@ -0,0 +1,34 @@
+package org.micromanager.lightsheetmanager.model;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+/**
+ * Plugin settings that are not a part of the acquisition settings.
+ */
+public class PluginSettings {
+
+    private boolean isPollingPositions_ = true;
+
+    public void setPollingPositions(final boolean state) {
+        isPollingPositions_ = state;
+    }
+
+    public boolean isPollingPositions() {
+        return isPollingPositions_;
+    }
+
+    public String toJson() {
+        return new Gson().toJson(this);
+    }
+
+    public String toPrettyJson() {
+        final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(this);
+    }
+
+    public static PluginSettings fromJson(final String json) {
+        return new Gson().fromJson(json, PluginSettings.class);
+    }
+
+}
