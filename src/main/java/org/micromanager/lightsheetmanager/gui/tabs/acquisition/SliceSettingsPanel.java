7bc1d92f035a2236482e6d7de44f67544a695d56
Brandon <bls337@yahoo.com>
2026-03-05 22:23:56 -0800

    do not create dispim ui elements in scape mode for the slice setting panel

--- src/main/java/org/micromanager/lightsheetmanager/gui/tabs/acquisition/SliceSettingsPanel.java
+++ src/main/java/org/micromanager/lightsheetmanager/gui/tabs/acquisition/SliceSettingsPanel.java
@@ -1,6 +1,7 @@
 package org.micromanager.lightsheetmanager.gui.tabs.acquisition;
 
 import org.micromanager.lightsheetmanager.api.data.CameraMode;
+import org.micromanager.lightsheetmanager.api.data.GeometryType;
 import org.micromanager.lightsheetmanager.api.internal.DefaultSliceSettings;
 import org.micromanager.lightsheetmanager.api.internal.DefaultSliceSettingsLS;
 import org.micromanager.lightsheetmanager.gui.components.CheckBox;
@@ -11,7 +12,6 @@ import org.micromanager.lightsheetmanager.LightSheetManager;
 
 import java.util.Objects;
 
-// TODO: virtual slit panel not needed in SCAPE mode
 public class SliceSettingsPanel extends Panel {
 
     // regular panel
@@ -47,12 +47,7 @@ public class SliceSettingsPanel extends Panel {
                 "[]5[]"
         );
 
-        final DefaultSliceSettingsLS sliceSettingsLS = model_.acquisitions()
-                .settings().sliceSettingsLS();
-
-        final DefaultSliceSettings sliceSettings = model_.acquisitions()
-                .settings().sliceSettings();
-
+        final DefaultSliceSettings sliceSettings = model_.acquisitions().settings().sliceSettings();
         final boolean isSlicePeriodMinimized = sliceSettings.isSlicePeriodMinimized();
 
         // regular panel
@@ -70,19 +65,23 @@ public class SliceSettingsPanel extends Panel {
             spnSlicePeriod_.setEnabled(false);
         }
 
-        // virtual slit panel
-        lblScanResetTime_ = new Label("Scan Reset Time [ms]:");
-        lblScanSettleTime_ = new Label("Scan Settle Time [ms]:");
-        lblShutterWidth_ = new Label("Shutter Width [µs]:");
-        lblShutterSpeed_ = new Label("1 / (shutter speed):");
-        spnScanResetTime_ = Spinner.createDoubleSpinner(
-                sliceSettingsLS.scanResetTime(), 1.0, 100.0, 0.25);
-        spnScanSettleTime_ = Spinner.createDoubleSpinner(
-                sliceSettingsLS.scanSettleTime(), 0.25, 100.0, 0.25);
-        spnShutterWidth_ = Spinner.createDoubleSpinner(
-                sliceSettingsLS.shutterWidth(),0.1, 100.0, 1.0);
-        spnShutterSpeed_ = Spinner.createDoubleSpinner(
-                sliceSettingsLS.shutterSpeedFactor(), 1.0, 10.0, 1.0);
+        if (model_.devices().adapter().geometry() == GeometryType.DISPIM) {
+            final DefaultSliceSettingsLS sliceSettingsLS = model_.acquisitions().settings().sliceSettingsLS();
+
+            // virtual slit panel
+            lblScanResetTime_ = new Label("Scan Reset Time [ms]:");
+            lblScanSettleTime_ = new Label("Scan Settle Time [ms]:");
+            lblShutterWidth_ = new Label("Shutter Width [µs]:");
+            lblShutterSpeed_ = new Label("1 / (shutter speed):");
+            spnScanResetTime_ = Spinner.createDoubleSpinner(
+                    sliceSettingsLS.scanResetTime(), 1.0, 100.0, 0.25);
+            spnScanSettleTime_ = Spinner.createDoubleSpinner(
+                    sliceSettingsLS.scanSettleTime(), 0.25, 100.0, 0.25);
+            spnShutterWidth_ = Spinner.createDoubleSpinner(
+                    sliceSettingsLS.shutterWidth(), 0.1, 100.0, 1.0);
+            spnShutterSpeed_ = Spinner.createDoubleSpinner(
+                    sliceSettingsLS.shutterSpeedFactor(), 1.0, 10.0, 1.0);
+        }
 
         // create the ui based on the camera trigger mode
         switchUI(model_.acquisitions().settings().cameraMode());
@@ -121,26 +120,29 @@ public class SliceSettingsPanel extends Panel {
             model_.acquisitions().recalculateSliceTiming();
         });
 
-        // virtual slit panel
-        spnScanResetTime_.registerListener(e -> {
-            model_.acquisitions().settingsBuilder()
-                    .sliceSettingsLSBuilder().scanResetTime(spnScanResetTime_.getDouble());
-        });
+        if (model_.devices().adapter().geometry() == GeometryType.DISPIM) {
 
-        spnScanSettleTime_.registerListener(e -> {
-            model_.acquisitions().settingsBuilder()
-                    .sliceSettingsLSBuilder().scanSettleTime(spnScanSettleTime_.getDouble());
-        });
+            // virtual slit panel
+            spnScanResetTime_.registerListener(e -> {
+                model_.acquisitions().settingsBuilder()
+                        .sliceSettingsLSBuilder().scanResetTime(spnScanResetTime_.getDouble());
+            });
 
-        spnShutterWidth_.registerListener(e -> {
-            model_.acquisitions().settingsBuilder()
-                    .sliceSettingsLSBuilder().shutterWidth(spnShutterWidth_.getDouble());
-        });
+            spnScanSettleTime_.registerListener(e -> {
+                model_.acquisitions().settingsBuilder()
+                        .sliceSettingsLSBuilder().scanSettleTime(spnScanSettleTime_.getDouble());
+            });
 
-        spnShutterSpeed_.registerListener(e -> {
-            model_.acquisitions().settingsBuilder()
-                    .sliceSettingsLSBuilder().shutterSpeedFactor(spnShutterSpeed_.getDouble());
-        });
+            spnShutterWidth_.registerListener(e -> {
+                model_.acquisitions().settingsBuilder()
+                        .sliceSettingsLSBuilder().shutterWidth(spnShutterWidth_.getDouble());
+            });
+
+            spnShutterSpeed_.registerListener(e -> {
+                model_.acquisitions().settingsBuilder()
+                        .sliceSettingsLSBuilder().shutterSpeedFactor(spnShutterSpeed_.getDouble());
+            });
+        }
     }
 
     /**
