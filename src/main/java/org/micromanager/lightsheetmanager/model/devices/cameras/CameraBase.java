a8b8cdde1d2ef64a2c7d50c6314c0e363d5fb5eb
Brandon <brandon@asiimaging.com>
2026-07-29 17:54:14 -0700

    getExposure should use device name as paramater to get specific camera exposure

--- src/main/java/org/micromanager/lightsheetmanager/model/devices/cameras/CameraBase.java
+++ src/main/java/org/micromanager/lightsheetmanager/model/devices/cameras/CameraBase.java
@@ -31,10 +31,13 @@ public abstract class CameraBase extends DeviceBase implements LightSheetCamera 
         }
     }
 
+    /**
+     * Returns this camera's exposure in milliseconds.
+     */
     public double getExposure() {
         double exposure = 0.0;
         try {
-            exposure = core_.getExposure();
+            exposure = core_.getExposure(deviceName_);
         } catch (Exception e) {
             studio_.logs().logError("could not get camera exposure");
         }
